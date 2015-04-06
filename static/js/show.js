78dcbfee1bcf1a3b7100ba417e41315e7ab6fb3e
tajima <niryuu@gmail.com>
2015-04-06 13:35:27 +0900

    dot by dot

--- static/js/show.js
+++ static/js/show.js
@@ -4,12 +4,13 @@ var loaded = 1;
 var last = 0;
 var loadPage = function(page) {
   var scale = 1.0;
-  var viewport = page.getViewport($('#pdfview').width()/page.getViewport(1.0).width);
+  var viewport = page.getViewport($('#pdfview').width()/page.getViewport(1.0).width * window.devicePixelRatio);
 
   var canvas = document.createElement('canvas');
   var context = canvas.getContext('2d');
   canvas.height = viewport.height;
   canvas.width = viewport.width;
+  canvas.style.cssText = 'width: ' + (viewport.width / window.devicePixelRatio) + 'px;'
 
   var renderContext = {
     canvasContext: context,
